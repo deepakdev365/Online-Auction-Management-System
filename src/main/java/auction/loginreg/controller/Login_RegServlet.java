013a1819850cb866302b71a3c9170dc9678d1da4
Deepak Kumar Sahu <deepak3650901@gmail.com>
2025-10-07 00:29:45 +0530

    Update and rename Login_RegServelet.java to Login_RegServlet.java and also changes the logic in the Login_RegServlet.java

--- src/main/java/auction/loginreg/controller/Login_RegServlet.java
+++ src/main/java/auction/loginreg/controller/Login_RegServlet.java
@@ -10,17 +10,10 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class Login_RegServelet
  */
-@WebServlet("/Login_RegServelet")
-public class Login_RegServelet extends HttpServlet {
+@WebServlet("/")
+public class Login_RegServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Login_RegServelet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
