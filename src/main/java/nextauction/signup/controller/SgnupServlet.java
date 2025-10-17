acc978b54f5987f6789e8f0958e5a2942ce0cda4
Shibananda061 <shibanandaswain290@gmail.com>
2025-10-17 19:36:51 +0530

    Initial commit

--- src/main/java/nextauction/signup/controller/SgnupServlet.java
+++ src/main/java/nextauction/signup/controller/SgnupServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class SgnupServlet
  */
-@WebServlet("/Sgnup")
+@WebServlet("/Signup")
 public class SgnupServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
