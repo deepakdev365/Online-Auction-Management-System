1080cdfe86c3b92eb7193acf726e201a7ded9631
deepakdev <deepak3650901@gmail.com>
2025-11-04 00:47:03 +0530

    resolved the web.xml, localhost problem

--- src/main/java/nextauction/util/DBUtil.java
+++ src/main/java/nextauction/util/DBUtil.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBUtil {
-
+    
     private static final String URL = "jdbc:mysql://localhost:3306/auction"; 
     private static final String USER = "root"; 
     private static final String PASSWORD = "Buster365@"; 
@@ -21,7 +21,9 @@ public class DBUtil {
         Connection conn = null;
         try {
             conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Database connected successfully!");
         } catch (Exception e) {
+            System.out.println("Database connection failed!");
             e.printStackTrace();
         }
         return conn;
